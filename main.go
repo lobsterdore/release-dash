3b635b736e19c778f80c20af07d93e2fbde48e4a
David Reed <david.reedfrancis@sainsburys.co.uk>
2020-08-16 16:31:59 +0100

    Fiddling with Git lib

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-github/v32/github"
+	"golang.org/x/oauth2"
+)
 
 func main() {
-	fmt.Println("Test")
+	ctx := context.Background()
+
+	gh_pat := os.Getenv("GH_PAT")
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: gh_pat},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	repo, _, err := client.Repositories.Get(ctx, "JSainsburyPLC", "api-issa-canary")
+
+	if err != nil {
+		if _, ok := err.(*github.RateLimitError); ok {
+			log.Println("hit rate limit")
+		}
+
+		fmt.Println("Bang")
+		os.Exit(1)
+	}
+
+	fmt.Println(repo)
 }
